93d30d361b71ecfa6984588a995a68b4248156d9
hyoeunlike@naver.com <hyoeunlike@naver.com>
2025-09-27 15:09:46 +0900

    0927 Box plot show

--- heart/heart.py
+++ heart/heart.py
@@ -37,3 +37,36 @@ names = X_data.columns
 print(names)
 
 # 데이터를 학습용과 평가용으로 분리
+X_train, X_test, Y_train, Y_test = \
+    train_test_split(X_data, Y_data,
+                     test_size=0.3)
+
+# 최종 데이터 모양
+print('\n학습용 입력 데이터 모양:', X_train.shape)
+print('학습용 출력 데이터 모양', Y_train.shape)
+print('평가용 입력 데이터 모양:', X_test.shape)
+print('평가용 출력 데이터 모양:', Y_test.shape)
+
+# 입력 데이터 Z-점수 정규화
+# 결과는 numpy의 n-차원 행렬 형식
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+print('전환 전:', type(X_train))
+
+# numpy에서 pandas로 전환
+# header 정보 복구 필요
+X_train = pd.DataFrame(X_train, columns=names)
+X_test = pd.DataFrame(X_test, columns=names)
+print('전환 후:', type(X_train))
+
+# 정규화 된 학습용 데이터 출력
+print('\n정규화 된 학습용 데이터 샘플 10개')
+print(X_train.head(10))
+print('정규화 된 학습용 데이터 통계')
+print(X_train.describe())
+
+# 학습용 데이터 상자 그림
+sns.set(font_scale=2)
+sns.boxplot(data=X_train, palette="colorblind")
+plt.show()
